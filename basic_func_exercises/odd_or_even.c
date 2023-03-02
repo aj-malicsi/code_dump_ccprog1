7b5f9b56b6b2146b35ce9bff06b15f5d86ae9a83
ajmalicsi <andre_malicsi@dlsu.edu.ph>
2023-03-02 18:54:38 +0800

    gumagana pero pag lumalabas yung prompt may naligaw na E before the answer

--- basic_func_exercises/odd_or_even.c
+++ basic_func_exercises/odd_or_even.c
@@ -5,11 +5,26 @@
 // write a function that takes an integer and returns "E" if it is even and "O" if it is odd
 // hint: use % (modulo) operator
 
-
-int main() {
+int odd_or_even(int val1)
+{
+	
+	if (val1 % 2 == 0) {
+		printf("E");
+	} else {
+		printf("O");
+	}
+	return 0;
+}
+int main() 
+{
+	int num;
+	printf("Input a value: ");
+	scanf("%d", &num);
+	printf("The value is: ", odd_or_even(num));
+	
+	odd_or_even(num);
 	
 
-	
 
   return 0;
 }
