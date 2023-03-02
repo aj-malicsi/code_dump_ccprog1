65dd45c7db8019990e897318ad880403a7c3c5de
Andre Malicsi <aj.malicsi@yahoo.com.ph>
2023-03-02 17:45:39 +0800

    add exercise

---
+++ basic_func_exercises/odd_or_even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+// write a function that takes an integer and returns "E" if it is even and "O" if it is odd
+// hint: use % (modulo) operator
+
+
+int main() {
+	
+
+	
+
+  return 0;
+}
+
+
